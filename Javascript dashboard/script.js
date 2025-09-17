b8144228634ec33d9c0c91064284551b15fd60a7
ebubebaleiyiegbu-html <ebubebaleiyiegbu@gmail.com>
2025-09-17 11:20:42 +0100

    Add files via upload

---
+++ Javascript dashboard/script.js
@@ -0,0 +1,52 @@
+// 1.
+let name = "Ebubechukwu";
+console.log(name);
+
+// 2. Favorite number
+const favoriteNumber = 8;
+console.log(favoriteNumber);
+
+// 3. Swap values
+let a = 5;
+let b = 10;
+let temp = a;
+a = b;
+b = temp;
+console.log("a =", a, "b =", b);
+
+// 4. Simple addition
+let x = 7;
+let y = 3;
+let sum = x + y;
+console.log("Sum =", sum);
+
+// 5. Age in 10 years
+let age = 20;
+let futureAge = age + 10;
+console.log("In 10 years, you will be", futureAge);
+
+// 6. Temperature in Celsius
+let temperature = 25;
+console.log(`Today's temperature is ${temperature}°C`);
+
+// 7. Full name
+let firstName = "Samantha";
+let lastName = "Chavez";
+let fullName = firstName + " " + lastName;
+console.log("Full name:", fullName);
+
+// 8. Area of a rectangle
+const length = 10;
+const width = 5;
+const area = length * width;
+console.log("Area of rectangle:", area);
+
+// 9. Days in a week
+const daysInWeek = 7;
+console.log("There are " + daysInWeek + " days in a week");
+
+// 10. Bank balance
+let balance = 100;
+let deposit = 50;
+let newBalance = balance + deposit;
+console.log("New balance is:", newBalance);
